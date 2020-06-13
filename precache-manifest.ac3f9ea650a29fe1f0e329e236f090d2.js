120baa3d8fa6f3310546692757b7eff413d97666
Bikrant <bikrant2000kumar@gmail.com>
2020-06-13 23:25:05 +0530

    Updates

---
+++ precache-manifest.ac3f9ea650a29fe1f0e329e236f090d2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ede67ddb94b977d8524d57f7c69e559f",
+    "url": "/launch/index.html"
+  },
+  {
+    "revision": "1c8dd0e7050d4823a1b4",
+    "url": "/launch/static/css/2.75329cac.chunk.css"
+  },
+  {
+    "revision": "aa33f2360d4b0e332399",
+    "url": "/launch/static/css/main.cd45fa90.chunk.css"
+  },
+  {
+    "revision": "1c8dd0e7050d4823a1b4",
+    "url": "/launch/static/js/2.a1ea0cd4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/launch/static/js/2.a1ea0cd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa33f2360d4b0e332399",
+    "url": "/launch/static/js/main.6c6c8498.chunk.js"
+  },
+  {
+    "revision": "eb70c6c6028f3c22daa1",
+    "url": "/launch/static/js/runtime-main.75c2adca.js"
+  },
+  {
+    "revision": "eff0e4c10001c3e9fc687a2174331ecf",
+    "url": "/launch/static/media/bee.eff0e4c1.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/launch/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/launch/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/launch/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/launch/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/launch/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
