a812a677e7d217f0e0bf7c8a110bf0fc5edc5fdf
Bikrant <bikrant2000kumar@gmail.com>
2020-07-28 19:03:49 +0530

    Updates

---
+++ precache-manifest.dae1dec761138f8046aa9fb4cd6b5a54.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1176161eb48ae0f21f6552ba56c2d90",
+    "url": "/launch/index.html"
+  },
+  {
+    "revision": "8a624d16689d10059180",
+    "url": "/launch/static/css/2.75329cac.chunk.css"
+  },
+  {
+    "revision": "33047e246451ec8eb02d",
+    "url": "/launch/static/css/main.ed92e536.chunk.css"
+  },
+  {
+    "revision": "8a624d16689d10059180",
+    "url": "/launch/static/js/2.0396dd60.chunk.js"
+  },
+  {
+    "revision": "f48c3d5916bf6774fac50d6d07efdb0d",
+    "url": "/launch/static/js/2.0396dd60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33047e246451ec8eb02d",
+    "url": "/launch/static/js/main.34b9c4bf.chunk.js"
+  },
+  {
+    "revision": "eb70c6c6028f3c22daa1",
+    "url": "/launch/static/js/runtime-main.75c2adca.js"
+  },
+  {
+    "revision": "eff0e4c10001c3e9fc687a2174331ecf",
+    "url": "/launch/static/media/bee.eff0e4c1.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/launch/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/launch/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/launch/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/launch/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/launch/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "9a76e74e107e6163dc82116b8236c132",
+    "url": "/launch/static/media/login.9a76e74e.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/launch/precache-manifest.d0bdb7dd779155ee2a937c1354d4e392.js"
+  "/launch/precache-manifest.dae1dec761138f8046aa9fb4cd6b5a54.js"
 );
 
 self.addEventListener('message', (event) => {
